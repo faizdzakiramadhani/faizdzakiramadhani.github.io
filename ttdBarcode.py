cad7f7cf1a60b45f399976e1601f3c866d2a88a4
faizdzakiramadhani <faizdzaki2410@gmail.com>
2025-01-03 20:11:11 +0700

    barcode

--- ttdBarcode.py
+++ ttdBarcode.py
@@ -1,15 +1,14 @@
-import pyautogui as agui
+
 import barcode
 from barcode.writer import ImageWriter
 from PIL import Image
 
 nm = ""
-gambar = "https://kudav5.github.io/"
+gambar = "https://faizdzakiramadhani.github.io/"
 
 ean = barcode.get('code128', gambar, writer=ImageWriter())
-nama = agui.prompt('Masukkan nama barcode: ')
+nama = input("Masukkan nama barcode")
 ean.save(nama)
 ean = Image.open(nama + ".png")
 ean.show()
 
-agui.alert('Gambar sudah disimpan sebagai ' + nama + '.png')
